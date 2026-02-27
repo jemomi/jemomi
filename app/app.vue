c8db15e3306a6a2dd76da583a61aebad1e844e64
Jens Morten Mikkelsen <dkjemomi@gmail.com>
2026-02-27 23:24:22 +0100

    styling main navigation and spacing for main-content section

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,22 @@
 <template>
+  <NuxtRouteAnnouncer />
   <header class="bg-zinc-900">
-    <nav class="container mx-auto">
-      <NuxtLink to="/">
+    <nav class="flex container mx-auto gap-8 py-2">
+      <NuxtLink
+        to="/"
+        class="p-4 hover:bg-zinc-500/50 transition-colors rounded-lg"
+      >
         Home
       </NuxtLink>
-      <NuxtLink to="/mit-id-status">
+      <NuxtLink
+        to="/mit-id-status"
+        class="p-4 hover:bg-zinc-500/50 transition-colors rounded-lg"
+      >
         MitID status
       </NuxtLink>
     </nav>
   </header>
-  <div>
-    <NuxtRouteAnnouncer />
+  <div class="mt-10">
     <NuxtPage />
   </div>
 </template>
