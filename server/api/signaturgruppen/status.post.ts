69bd0cc0a6f9c259d79083a0f87552c7202d15d3
Jens Morten Mikkelsen <dkjemomi@gmail.com>
2026-02-28 00:37:38 +0100

    impl saving status report to neonDB

--- server/api/signaturgruppen/status.post.ts
+++ server/api/signaturgruppen/status.post.ts
@@ -1,7 +1,34 @@
+import { neon } from '@neondatabase/serverless';
+
 export default defineEventHandler(async (event) => {
-    console.log(`Received event: ${JSON.stringify(event)}`);
+    const dbUrl = process.env.DATABASE_URL;
+    if (!dbUrl) {
+        throw createError({statusCode: 500, statusMessage: "Missing DATABASE_URL"});
+    }
+
+    const raw = await readRawBody(event);
+    if (!raw) {
+        throw createError({statusCode: 400, statusMessage: "Missing body"});
+    }
+
+    let payload: any; //eslint-disable-line
+    try {
+        payload = JSON.parse(raw);
+    } catch {
+        throw createError({ statusCode: 400, statusMessage: "Invalid JSON" });
+    }
+
+    const eventType = payload?.meta?.event_type ?? payload?.event_type ?? null;
+    const headers = getHeaders(event);
+
+    const sql = neon(dbUrl);
+    await sql`
+    insert into signatur_events (event_type, payload, headers)
+    values (${eventType}, ${payload}, ${headers})
+  `;
 
     return {
         statusCode: 200,
+        ok: true,
     }
 })
