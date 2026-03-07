f44c567346e11b8b7d72f6f6ec44bf579b71c16c
Jens Morten Mikkelsen <dkjemomi@gmail.com>
2026-03-07 23:44:36 +0100

    init me.vue

---
+++ app/pages/me.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="container mx-auto">
+    <h1>
+      Hello 👋
+      <span>My name is</span> <!-- switching translation, jeg hedder, je suis, ich bin others... -->
+      Jens Morten Mikkelsen
+    </h1>
+<!--    <p>
+      This is you.
+      &lt;!&ndash; fun place to show available information about the visitor 😈&ndash;&gt;
+    </p>-->
+    <section>
+      <h2>
+        Music! 🎶
+      </h2>
+      <p>
+        I love music, and it has always been a huge part of my life!
+      </p>
+    </section>
+  </article>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
