126a6658b3ee720b0323e6f2c1c65c6d3db4891b
Jens Morten Mikkelsen <dkjemomi@gmail.com>
2026-03-10 00:41:06 +0100

    init endpoint for favorite-artists.get.ts

--- server/api/spotify/favorite-artists.get.ts
+++ server/api/spotify/favorite-artists.get.ts
@@ -1,8 +1,27 @@
+import {getSpotifyAccessToken} from '#server/utils/spotify';
+import type {SpotifyArtist} from '#shared/types/spotify';
+
 export default defineEventHandler(async (event) => {
     // get list from DB
 
     const artistIds = [
-        '1WNoKxsp715jez1Td4vthc'
+        '1WNoKxsp715jez1Td4vthc',
+        '2n2RSaZqBuUUukhbLlpnE6',
     ]
 
+    const result = []
+
+    const token = await getSpotifyAccessToken()
+
+    for await (const item of artistIds) {
+        const res = await $fetch<SpotifyArtist>('https://api.spotify.com/v1/artists/' + item, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${ token }`
+            }
+        })
+        result.push(res)
+    }
+
+    return result
 })
